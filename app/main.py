f802da3aaf742f5149fb1c13a8fd30e3623e9660
bafodej <bafode.jaiteh4@gmail.com>
2025-11-20 13:35:13 +0100

    add evedently metrics

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .routes.prediction_route import router as prediction_router 
 from .services.prediction_services import prediction_service
+from .routes.evidently_metrics_route import router as metrics_router
 import logging
 
 # Configuration du logging
@@ -67,3 +68,6 @@ async def health():
         "status": "healthy" if prediction_service.is_loaded() else "degraded",
         "model_loaded": prediction_service.is_loaded()
     }
+
+app.include_router(metrics_router)
+
